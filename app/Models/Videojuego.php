fa156bfffa4c5c4a0629fe774a3b66f591b33dbb
AndreyAraya <arayaquiroscarlosandrey@gmail.com>
2026-05-08 00:56:14 -0600

    Base creada y 10 juegos agregados

---
+++ app/Models/Videojuego.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Videojuego extends Model
+{
+    // Le confirmamos a Laravel el nombre de la tabla, porque normalmente lo busca como Videojuegos, con s al final
+    protected $table = 'videojuegos';
+
+    // Campos para usar en el backend y frontend
+    protected $fillable = [
+        'titulo',
+        'descripcion',
+        'genero',
+        'plataforma',
+        'precio',
+        'stock',
+        'fecha_lanzamiento'
+    ];
+}
